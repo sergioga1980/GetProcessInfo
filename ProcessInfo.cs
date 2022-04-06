5d6a49a5bf7421c0b950e59cd3de7dd7ececdc3e
sergioga1980 <101757756+sergioga1980@users.noreply.github.com>
2022-04-06 09:31:29 +0200

    Add files via upload

---
+++ ProcessInfo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ProcessInfo
+    {
+        private string pid; // field
+        private string parentId; // field
+        private string osName;
+        private string name;
+        private string path;
+        private string userSID;
+        private string cmdLine;
+        private string handle;
+        private string creationDate;
+        private string caption;
+        private string csName;
+
+
+        public string PID   // property
+        {
+            get { return pid; }   // get method
+            set { pid = value; }  // set method
+        }
+
+        public string Handle   // property
+        {
+            get { return handle; }   // get method
+            set { handle = value; }  // set method
+        }
+
+        public string CreationDate   // property
+        {
+            get { return creationDate; }   // get method
+            set { creationDate = value; }  // set method
+        }
+
+        public string Caption   // property
+        {
+            get { return caption; }   // get method
+            set { caption = value; }  // set method
+        }
+
+        public string CsName   // property
+        {
+            get { return csName; }   // get method
+            set { csName = value; }  // set method
+        }
+
+        public string ParentId   // property
+        {
+            get { return parentId; }   // get method
+            set { parentId = value; }  // set method
+        }
+
+        public string OSName   // property
+        {
+            get { return osName; }   // get method
+            set { osName = value; }  // set method
+        }
+
+        public string Name   // property
+        {
+            get { return name; }   // get method
+            set { name = value; }  // set method
+        }
+
+        public string Path   // property
+        {
+            get { return path; }   // get method
+            set { path = value; }  // set method
+        }
+
+        public string UserSID   // property
+        {
+            get { return userSID; }   // get method
+            set { userSID = value; }  // set method
+        }
+
+        public string CmdLine   // property
+        {
+            get { return cmdLine; }   // get method
+            set { cmdLine = value; }  // set method
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics.Eventing;
+using Microsoft.Windows.EventTracing;
+using Microsoft.Windows.EventTracing.Processes;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Process");
+            ManagementObjectCollection collection = searcher.Get();
+
+            string path = string.Empty;
+            string name = string.Empty;
+            string pid = string.Empty;
+            string parent_id = string.Empty;
+            string processname = String.Empty;
+            string userSID = String.Empty;
+            string cmdLine = String.Empty;
+            string osName = String.Empty;
+            string creationDate = String.Empty;
+            string csName = String.Empty;
+            string caption = String.Empty;
+            string handle = String.Empty;
+            string hash = String.Empty;
+
+
+            foreach (ManagementObject item in collection)
+                        {
+                            ProcessInfo processInfoNew = new ProcessInfo();
+                            string[] outParameters = new String[2];
+                            try
+                            {
+                                pid = Convert.ToString(item["ProcessId"]);
+                                parent_id = Convert.ToString(item["ParentProcessId"]);               
+                                path = Convert.ToString(item["ExecutablePath"]);
+                                name = Convert.ToString(item["Name"]);
+                                item.InvokeMethod("GetOwnerSid", (object[])outParameters);
+                                userSID = (string)outParameters[0];
+                                cmdLine = Convert.ToString(item["CommandLine"]);
+                                osName = Convert.ToString(item["OSName"]);
+                                creationDate = Convert.ToString(item["CreationDate"]);
+                                csName = Convert.ToString(item["CSName"]);
+                                caption = Convert.ToString(item["Caption"]);
+                                handle = Convert.ToString(item["Handle"]);
+                }
+                            catch (Exception ex)
+                            { }
+
+                using (var sha = new System.Security.Cryptography.SHA256Managed())
+                {
+                    byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(csName + "");
+                    byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                    // Convert back to a string, removing the '-' that BitConverter adds
+                     hash = BitConverter
+                        .ToString(hashBytes)
+                        .Replace("-", String.Empty);
+
+                 
+                    }
+
+                    processInfoNew.PID = pid;
+                            processInfoNew.ParentId = parent_id;
+                            processInfoNew.OSName = osName;
+                            processInfoNew.Name = name;
+                            processInfoNew.Path = path;
+                            processInfoNew.UserSID = userSID;
+                            processInfoNew.CreationDate = creationDate;
+                            processInfoNew.CmdLine = cmdLine;
+                            processInfoNew.CsName = hash;
+                            processInfoNew.Caption = caption;
+                            processInfoNew.Handle = handle;
+
+
+                string jsonString = JsonSerializer.Serialize(processInfoNew);
+
+                            string pathToStore = @"c:\Desarrollo\test.json";
+
+                            using (var tw = new StreamWriter(pathToStore, true))
+                            {
+                                tw.WriteLine(jsonString.ToString());
+                                tw.Close();
+                            }
+
+                        }
+
+            // EventLogSession session = new EventLogSession();
+
+            // var providers = session.GetProviderNames().ToList();
+
+      
+        
+
+
+
+
+        Console.ReadLine();
+        }
+    }
+}
